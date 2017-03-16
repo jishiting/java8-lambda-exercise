8e0c87fd4e85bd7cd37fb5d812a40a630f27fcce
jishiting <312683897@qq.com>
2017-03-16 08:16:46 +0800

    flatmap reduced

--- project1 helloLambda/src/main/java/StreamDemo.java
+++ project1 helloLambda/src/main/java/StreamDemo.java
@@ -1,20 +1,19 @@
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * Created by shitingji on 09/03/2017.
- */
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+
 public class StreamDemo {
     public static void main(String[] args) {
         //Collectors.toList()
-        List<String> collected = Stream.of("a", "b", "c").collect(Collectors.toList());
-        boolean isBoolean = Arrays.asList("a", "b", "c").equals(collected);
+        List<String> collected = Stream.of("a", "b", "c").collect(toList());
+        boolean isBoolean = asList("a", "b", "c").equals(collected);
         System.out.println("the result is -->" + isBoolean);
 
         //map
-        collected = Stream.of("a", "b", "c").map(string -> string.toUpperCase()).collect(Collectors.toList());
+        collected = Stream.of("a", "b", "c").map(string -> string.toUpperCase()).collect(toList());
         collected.forEach(x -> System.out.println("after uppercase" + x));
 
         //filter
@@ -26,8 +25,14 @@ public class StreamDemo {
                 }
 
 
-        ).collect(Collectors.toList());
+        ).collect(toList());
         collected.forEach(x -> System.out.println("after filter -->" + x));
+        // flatMap
+        List<Integer> together = Stream.of(asList(1, 2), asList(3, 4)).flatMap(numbers -> numbers.stream()).collect(toList());
+        together.forEach(x -> System.out.println("after flatMap -->" + x));
+        //reduce
+        int count = Stream.of(1, 2, 3).reduce(0, (acc, element) -> acc + element);//0是累加器acc的初始值
+        System.out.println("after reduce --> " + count);
 
 
     }
